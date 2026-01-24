20e5b79c8945233c0499e8a90876bdbc276d271b
siddharthasiddu11 <siddullll6@gmail.com>
2026-01-24 23:41:24 +0530

    updated page.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const HeroSection = () => {
+      const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return <section className='w-full pt-36 md:pt-48 pb-10'>
     <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
@@ -12,7 +33,7 @@ const HeroSection = () => {
             </h1>
             <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.</p>
         </div>
-        <div>
+        <div className='flex justify-center space-x-4'>
             <Link href={"/dashboard"}>
                <Button size='lg' className="px-8">Get Started</Button>
             </Link>
@@ -20,8 +41,8 @@ const HeroSection = () => {
              <Button size='lg' variant='outline'>Watch Demo</Button>
             </Link>
         </div>
-        <div>
-            <div>
+        <div className='hero-image-wrapper mt-5 md:mt-0'>
+            <div ref={imageRef} className='hero-image'>
                 <Image
                 src={'/banner.png'}
                 width={1280}
