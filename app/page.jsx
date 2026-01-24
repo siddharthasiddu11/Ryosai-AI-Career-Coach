9a09e38cc7aeac95e4a17111a9afd5dc76e35f6d
siddharthasiddu11 <siddullll6@gmail.com>
2026-01-24 23:57:34 +0530

    added card

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ import HeroSection from "@/components/hero";
 import { Card, CardContent } from "@/components/ui/card";
 import { features } from "@/data/feature";
 import { howItWorks } from "@/data/howItWorks";
+import { testimonial } from "@/data/testinomials";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -84,6 +86,28 @@ export default function Home() {
             ))}
           </div>
         </div>
+      </section>
+      
+      {/* What Our Users Say */}
+      <section className="w-full py-12 md:py-24 bg-muted/50">
+       <div className="container mx-auto px-4 md:px-6">
+        <h2 className="text-3xl font-bold text-center mb-12">What Our Users Say</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+         {testimonial.map((testimonial, index) => {
+          <Card key={index} className="bg-background">
+            <CardContent className="pt-6">
+              <div className="flex flex-col space-y-4">
+                <div className="flex items-center space-x-4 mb-4">
+                  <div>
+                   <Image width={40} height={40} src={testimonial.image} alt={testimonial.author} className="rounded-full object-cover border-2 border-primary/20"/>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+         })}
+        </div>
+       </div>
       </section>
     </div>
    
