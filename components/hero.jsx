2392a0a8980f1a2555878e09a44b9c645e90f819
siddharthasiddu11 <siddullll6@gmail.com>
2026-01-24 22:03:28 +0530

    updated hero

--- components/hero.jsx
+++ components/hero.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
             < h1 className="text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
                 Your AI Career Coach for <br /> Professional Success
             </h1>
-            <p>Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.</p>
+            <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.</p>
         </div>
         <div>
             <Link href={"/dashboard"}>
