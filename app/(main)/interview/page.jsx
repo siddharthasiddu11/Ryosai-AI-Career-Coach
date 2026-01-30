50404fdfc91c83295996f1622e9be0f9d2a8c7e4
siddharthasiddu11 <siddullll6@gmail.com>
2026-01-30 23:31:57 +0530

    Interview page

---
+++ app/(main)/interview/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">Interview Preparation</h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
